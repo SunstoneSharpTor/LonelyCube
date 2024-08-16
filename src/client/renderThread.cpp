9499adfed4f8d94dbca6ba459d262d5c50ead94c
Bertie Cartwright <bertiecartwright@tutamail.com>
2024-08-16 10:47:44 +0100

    Began rendering the world to a texture using a framebuffer and then displaying the framebuffer by drawing it to a full-screen quad

--- src/client/renderThread.cpp
+++ src/client/renderThread.cpp
@@ -113,12 +113,14 @@ void RenderThread::go(bool* running) {
     Shader blockShader("res/shaders/basicVertex.txt", "res/shaders/basicFragment.txt");
     blockShader.bind();
 
+    //screen shader
+    Shader screenShader("res/shaders/screenShaderVertex.txt", "res/shaders/screenShaderFragment.txt");
+    waterShader.bind();
+
     Texture allBlockTextures("res/blockTextures.png");
     allBlockTextures.bind();
     blockShader.setUniform1i("u_texture", 0);
     blockShader.setUniform1f("u_renderDistance", (m_mainWorld->getRenderDistance() - 1) * constants::CHUNK_SIZE);
-
-    glClearColor(0.57f, 0.70f, 1.0f, 1.0f);
 
     Renderer mainRenderer;
 
@@ -131,21 +133,28 @@ void RenderThread::go(bool* running) {
                                        -1.0f, -8.0f,
 
                                        -8.0f,  1.0f,
-                                        8.0f,  1.0f,
-                                        8.0f, -1.0f,
-                                       -8.0f, -1.0f };
+                                       -1.0f,  1.0f,
+                                       -1.0f, -1.0f,
+                                       -8.0f, -1.0f,
 
-    unsigned int crosshairIndices[12] = { 2, 1, 0,
-                                          0, 3, 2,
-                                          6, 5, 4,
-                                          4, 7, 6 };
+                                        8.0f,  1.0f,
+                                        1.0f,  1.0f,
+                                        1.0f, -1.0f,
+                                        8.0f, -1.0f };
+
+    unsigned int crosshairIndices[18] = { 2,  1,  0,
+                                          0,  3,  2,
+                                          6,  5,  4,
+                                          4,  7,  6,
+                                         10,  8,  9,
+                                          8, 10, 11 };
 
     VertexArray crosshairVA;
     VertexBuffer crosshairVB(crosshairCoordinates, 24 * sizeof(float));
     VertexBufferLayout crosshairVBlayout;
     crosshairVBlayout.push<float>(2);
     crosshairVA.addBuffer(crosshairVB, crosshairVBlayout);
-    IndexBuffer crosshairIB(crosshairIndices, 12);
+    IndexBuffer crosshairIB(crosshairIndices, 18);
 
     Shader crosshairShader("res/shaders/crosshairVertex.txt", "res/shaders/crosshairFragment.txt");
     crosshairShader.bind();
@@ -161,6 +170,36 @@ void RenderThread::go(bool* running) {
     IndexBuffer blockOutlineIB(constants::CUBE_WIREFRAME_IB, 16);
 
     Shader blockOutlineShader("res/shaders/wireframeVertex.txt", "res/shaders/wireframeFragment.txt");
+
+    // Set up frame buffer
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    unsigned int textureColourbuffer;
+    glGenTextures(1, &textureColourbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColourbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, windowDimensions[0], windowDimensions[1], 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColourbuffer, 0);
+    unsigned int textureDepthBuffer;
+    glGenTextures(1, &textureDepthBuffer);
+    glBindTexture(GL_TEXTURE_2D, textureDepthBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, windowDimensions[0], windowDimensions[1], 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, textureDepthBuffer, 0);
+    float screenCoords[24] = { -1.0f, -1.0f, 0.0f, 0.0f,
+                                        1.0f,  -1.0f, 1.0f,  0.0f,
+                                        -1.0f, 1.0f, 0.0f, 1.0f,
+                                       -1.0f, 1.0f, 0.0f, 1.0f,
+                                       1.0f,  -1.0f, 1.0f,  0.0f,
+                                        1.0f,  1.0f, 1.0f,  1.0f };
+
+    VertexArray screenVA;
+    VertexBuffer screenVB(screenCoords, 24 * sizeof(float));
+    VertexBufferLayout screenVBlayout;
+    screenVBlayout.push<float>(2);
+    screenVBlayout.push<float>(2);
+    screenVA.addBuffer(screenVB, screenVBlayout);
 
     float cameraPos[3];
     cameraPos[0] = m_mainPlayer->cameraBlockPosition[0] + m_mainPlayer->viewCamera.position[0];
@@ -238,6 +277,10 @@ void RenderThread::go(bool* running) {
         }
         if (windowevent_resized) {
             SDL_GetWindowSize(sdl_window, &windowDimensions[0], &windowDimensions[1]);
+            glBindTexture(GL_TEXTURE_2D, textureColourbuffer);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, windowDimensions[0], windowDimensions[1], 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+            glBindTexture(GL_TEXTURE_2D, textureDepthBuffer);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, windowDimensions[0], windowDimensions[1], 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
             glViewport(0, 0, windowDimensions[0], windowDimensions[1]);
             crosshairProj = glm::ortho(-(float)windowDimensions[0] / 2, (float)windowDimensions[0] / 2, -(float)windowDimensions[1] / 2, (float)windowDimensions[1] / 2, -1.0f, 1.0f);
             crosshairShader.setUniformMat4f("u_MVP", crosshairProj);
@@ -303,22 +346,36 @@ void RenderThread::go(bool* running) {
                 lookingAtBlock = false;
             }
 
-            //draw background
-            mainRenderer.clear();
-            //m_mainWorld->renderChunks(mainRenderer, blockShader, waterShader, view, proj, m_mainPlayer->cameraBlockPosition, (float)windowDimensions[0] / (float)windowDimensions[1], FOV, actualDT);
+            glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
+            glClearColor(0.57f, 0.70f, 1.0f, 1.0f);
+            mainRenderer.clear();
+
+            allBlockTextures.bind();
             //auto tp1 = std::chrono::high_resolution_clock::now();
             m_mainWorld->renderChunks(mainRenderer, blockShader, waterShader, view, proj, m_mainPlayer->cameraBlockPosition, (float)windowDimensions[0] / (float)windowDimensions[1], FOV, actualDT);
             //auto tp2 = std::chrono::high_resolution_clock::now();
             //cout << std::chrono::duration_cast<std::chrono::microseconds>(tp2 - tp1).count() << "us\n";
-
             if (lookingAtBlock) {
                 mainRenderer.drawWireframe(blockOutlineVA, blockOutlineIB, blockOutlineShader);
             }
 
+            // second pass
+            glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            
+            glDisable(GL_DEPTH_TEST);
+            screenShader.bind();
+            screenVA.bind();
+            glActiveTexture(0);
+            glBindTexture(GL_TEXTURE_2D, textureColourbuffer);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
             glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
             mainRenderer.draw(crosshairVA, crosshairIB, crosshairShader);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_DEPTH_TEST);
 
             SDL_GL_SwapWindow(sdl_window);
 
@@ -342,6 +399,10 @@ void RenderThread::go(bool* running) {
 
     SDL_DestroyWindow(sdl_window);
     SDL_Quit();
+}
+
+void RenderThread::renderSky() {
+
 }
 
 }  // namespace client
