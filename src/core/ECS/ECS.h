1235b0cdc2baf0d71f6dd06859624ee3844546c9
Bertie Cartwright <bertiecartwright@tutamail.com>
2024-10-31 12:57:33 +0000

    Continued working on entity component system

--- src/core/ECS/ECS.h
+++ src/core/ECS/ECS.h
@@ -31,29 +31,84 @@ struct EntityDesc
     ComponentMask mask;
 };
 
+
+struct ComponentPool
+{
+    std::unique_ptr<char[]> pData;
+    size_t elementSize;
+    ComponentPool(size_t elementsize, int maxEntities) : elementSize(elementsize),
+        pData(std::make_unique<char[]>(elementSize * maxEntities)) {}
+
+    inline void* get(size_t index)
+    {
+        return pData.get() + index * elementSize;
+    }
+};
+
+
 class ECS {
 private: 
     inline static int m_componentCounter = 0;
     template<typename T>
-    inline static int m_componentID = m_componentCounter++;
+    inline static int s_componentID = m_componentCounter++;
+
+    int m_maxEntities;
     std::vector<EntityDesc> m_entities;
+    std::vector<std::unique_ptr<ComponentPool>> m_componentPools;
 
 public:
+    ECS(int maxEntities);
+
     template<typename T>
-    int getID() 
+    static int getID();
+
+    EntityID newEntity();
+
+    template<typename T>
+    T& assign(EntityID id);
+
+    template<typename T>
+    T& get(EntityID id);
+};
+
+
+ECS::ECS(int maxEntities) : m_maxEntities(maxEntities) {}
+
+template<typename T>
+int ECS::getID() 
+{
+    return s_componentID<T>;
+}
+
+EntityID ECS::newEntity()
+{
+    m_entities.push_back({ m_entities.size(), ComponentMask() });
+    return m_entities.back().id;
+}
+
+template<typename T>
+T& ECS::assign(EntityID id)
+{
+    int componentID = getID<T>();
+
+    if (m_componentPools.size() <= componentID) // New component, make a new pool
     {
-        return m_componentID<T>;
+        m_componentPools.resize(componentID + 1);
+        m_componentPools[componentID] = std::make_unique<ComponentPool>(sizeof(T), m_maxEntities);
     }
 
-    EntityID newEntity()
-    {
-        m_entities.push_back({ m_entities.size(), ComponentMask() });
-        return m_entities.back().id;
-    }
-    
-    template<typename T>
-    void assign(EntityID id)
-    {
-        m_entities[id].mask.set(getID<T>());
-    }
-};
+    // Looks up the component in the pool, and initializes it with placement new
+    T* component = new (m_componentPools[componentID]->get(id)) T();
+
+    // Set the bit for this component to true and return the created component
+    m_entities[id].mask.set(componentID);
+
+    return *component;
+}
+
+template<typename T>
+T& ECS::get(EntityID id)
+{
+    int componentID = getID<T>();
+    return *static_cast<T*>(m_componentPools[componentID]->get(id));
+}
