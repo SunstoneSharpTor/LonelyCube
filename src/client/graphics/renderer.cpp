2d5f0801c5700ac0d58c4af2c974b6b4bbefa7da
Bertie Cartwright <bertiecartwright@tutamail.com>
2025-01-12 22:09:30 +0000

    Update copyright year

--- src/client/graphics/renderer.cpp
+++ src/client/graphics/renderer.cpp
@@ -1,6 +1,6 @@
 /*
   Lonely Cube, a voxel game
-  Copyright (C) 2024 Bertie Cartwright
+  Copyright (C) g 2024-2025 Bertie Cartwright
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
