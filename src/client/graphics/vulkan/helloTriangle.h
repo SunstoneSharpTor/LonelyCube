b99fd3033698747a793c91a172a52d2f4eb5ab59
Bertie Cartwright <sunstonesharptor@gmail.com>
2025-01-17 18:25:03 +0000

    Added vulkan to the CMakeLists and created a vulkan instance

---
+++ src/client/graphics/vulkan/helloTriangle.h
@@ -0,0 +1,43 @@
+/*
+  Lonely Cube, a voxel game
+  Copyright (C) g 2024-2025 Bertie Cartwright
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+namespace lonelycube::client {
+
+class HelloTriangleApplication
+{
+public:
+    void run();
+
+private:
+    GLFWwindow* m_window;
+    VkInstance m_instance;
+
+    void initWindow();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+
+    void createInstance();
+};
+
+}  // namespace lonelycube::client
