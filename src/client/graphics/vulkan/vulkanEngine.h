e0721702cb744891fbdbb21bebd59fed992a4127
Bertie Cartwright <sunstonesharptor@gmail.com>
2025-01-31 13:00:38 +0000

    Began seperating out the code into an engine class to eventually allow for drawing a more complicated scene with vulkan

---
+++ src/client/graphics/vulkan/vulkanEngine.h
@@ -0,0 +1,132 @@
+/*
+  Lonely Cube, a voxel game
+  Copyright (C) g 2024-2025 Bertie Cartwright
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include "core/pch.h"
+
+namespace lonelycube::client {
+
+class VulkanEngine
+{
+public:
+    VulkanEngine();
+    bool init();
+    void cleanup();
+    bool drawFrame();
+
+    inline VkDevice getDevice()
+    {
+        return m_device;
+    }
+
+    inline GLFWwindow* getWindow()
+    {
+        return m_window;
+    }
+
+    inline void singalFramebufferResize()
+    {
+        m_framebufferResized = true;
+    }
+
+private:
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+        std::optional<uint32_t> presentFamily;
+    };
+
+    struct SwapChainSupportDetails
+    {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    const uint32_t m_WIDTH = 800;
+    const uint32_t m_HEIGHT = 600;
+    const int m_MAX_FRAMES_IN_FLIGHT = 2;
+
+    uint32_t m_currentFrame = 0;
+    GLFWwindow* m_window;
+    VkInstance m_instance;
+    const bool m_enableValidationLayers;
+    const std::vector<const char*> m_validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+    const std::vector<const char*> m_deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+    VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+    VkDevice m_device;
+    VkQueue m_graphicsQueue;
+    VkQueue m_presentQueue;
+    VkSurfaceKHR m_surface;
+    VkSwapchainKHR m_swapchain;
+    std::vector<VkImage> m_swapchainImages;
+    VkFormat m_swapchainImageFormat;
+    VkExtent2D m_swapchainExtent;
+    std::vector<VkImageView> m_swapchainImageViews;
+    std::vector<VkFramebuffer> m_swapchainFramebuffers;
+    VkRenderPass m_renderPass;
+    VkPipelineLayout m_pipelineLayout;
+    VkPipeline m_graphicsPipeline;
+    VkCommandPool m_commandPool;
+    std::vector<VkCommandBuffer> m_commandBuffers;
+    std::vector<VkSemaphore> m_imageAvailableSemaphores;
+    std::vector<VkSemaphore> m_renderFinishedSemaphores;
+    std::vector<VkFence> m_inFlightFences;
+    bool m_framebufferResized = false;
+
+    void initWindow();
+    bool initVulkan();
+
+    void cleanupSwapchain();
+    bool createInstance();
+    bool checkValidationLayerSupport();
+    bool pickPhysicalDevice();
+    int ratePhysicalDeviceSuitability(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    bool createLogicalDevice();
+    bool createSurface();
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(
+        const std::vector<VkSurfaceFormatKHR>& availableFormats
+    );
+    VkPresentModeKHR chooseSwapPresentMode(
+        const std::vector<VkPresentModeKHR>& availablePresentModes
+    );
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+    bool createSwapchain();
+    bool recreateSwapchain();
+    bool createImageViews();
+    bool createGraphicsPipeline();
+    bool createRenderPass();
+    bool createFramebuffers();
+    bool createCommandPool();
+    bool createCommandBuffers();
+    bool recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+    bool createSyncObjects();
+};
+
+}  // namespace lonelycube::client
