a310079ab30af6716601b8d37e65592969cae13c
Bertie Cartwright <sunstonesharptor@gmail.com>
2025-02-06 10:34:23 +0000

    Added first vulkan compute shatder

--- src/client/shaders/compile.sh
+++ src/client/shaders/compile.sh
@@ -1,2 +1,3 @@
 glslangValidator -V src/client/shaders/test.vert.glsl -o res/shaders/test.vert.spv
 glslangValidator -V src/client/shaders/test.frag.glsl -o res/shaders/test.frag.spv
+glslangValidator -V src/client/shaders/gradient.comp.glsl -o res/shaders/gradient.comp.spv
