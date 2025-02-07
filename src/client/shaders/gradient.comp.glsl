3dd1ba6cbede23c92c27b94167e628e3066bb396
Bertie Cartwright <bertiecartwright@tutamail.com>
2025-02-07 14:35:53 +0000

    Set up push constants for the compute shader

--- src/client/shaders/gradient.comp.glsl
+++ src/client/shaders/gradient.comp.glsl
@@ -22,14 +22,24 @@ layout (local_size_x = 16, local_size_y = 16) in;
 
 layout (rgba16f, set = 0, binding = 0) uniform image2D image;
 
+layout (push_constant) uniform constants
+{
+    vec4 data1;
+    vec4 data2;
+} PushConstants;
+
 void main()
 {
     ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);
     ivec2 size = imageSize(image);
 
+    vec4 topColour = PushConstants.data1;
+    vec4 bottomColour = PushConstants.data2;
+
     if (texelCoord.x < size.x && texelCoord.y < size.y)
     {
-        vec4 colour = vec4(0.0, 0.0, 0.0, 1.0);
+        float blend = float(texelCoord.y) / size.y;
+        vec4 colour = mix(topColour, bottomColour, blend);
 
         if (gl_LocalInvocationID.x != 0 && gl_LocalInvocationID.y != 0)
         {
