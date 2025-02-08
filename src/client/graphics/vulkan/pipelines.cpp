8cf75647f40522e14599950012efbaef8bd54204
Bertie Cartwright <sunstonesharptor@gmail.com>
2025-02-08 13:46:56 +0000

    Added a graphics pipeline that draws a triangle using dynamic rendering

---
+++ src/client/graphics/vulkan/pipelines.cpp
@@ -0,0 +1,168 @@
+/*
+  Lonely Cube, a voxel game
+  Copyright (C) g 2024-2025 Bertie Cartwright
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "client/graphics/vulkan/pipelines.h"
+
+#include <vulkan/vulkan_core.h>
+#include "core/log.h"
+
+namespace lonelycube::client {
+
+void PipelineBuilder::clear()
+{
+    inputAssembly = { .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO };
+    rasterizer = { .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO };
+    colourBlendAttachment = {};
+    multisampling = { .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO };
+    pipelineLayout = {};
+    depthStencil = { .sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO };
+    renderInfo = { .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO };
+
+    shaderStages.clear();
+}
+
+void PipelineBuilder::setShaders(VkShaderModule vertexShader, VkShaderModule fragmentShader)
+{
+    VkPipelineShaderStageCreateInfo vertexStageInfo{};
+    vertexStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertexStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertexStageInfo.module = vertexShader;
+    vertexStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragmentStageInfo{};
+    fragmentStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragmentStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragmentStageInfo.module = fragmentShader;
+    fragmentStageInfo.pName = "main";
+
+    shaderStages.clear();
+    shaderStages.push_back(vertexStageInfo);
+    shaderStages.push_back(fragmentStageInfo);
+}
+
+void PipelineBuilder::setInputTopology(VkPrimitiveTopology topology)
+{
+    inputAssembly.topology = topology;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+}
+
+void PipelineBuilder::setPolygonMode(VkPolygonMode mode)
+{
+    rasterizer.polygonMode = mode;
+    rasterizer.lineWidth = 1.0f;
+}
+
+void PipelineBuilder::setCullMode(VkCullModeFlags cullMode, VkFrontFace frontFace)
+{
+    rasterizer.cullMode = cullMode;
+    rasterizer.frontFace = frontFace;
+}
+
+void PipelineBuilder::setMultisamplingNone()
+{
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisampling.minSampleShading = 1.0f;
+    multisampling.alphaToCoverageEnable = VK_FALSE;
+    multisampling.alphaToOneEnable = VK_FALSE;
+}
+
+void PipelineBuilder::disableBlending()
+{
+    colourBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT
+        | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    colourBlendAttachment.blendEnable = VK_FALSE;
+}
+
+void PipelineBuilder::setColourAttachmentFormat(VkFormat format)
+{
+    colourAttachmentFormat = format;
+    renderInfo.colorAttachmentCount = 1;
+    renderInfo.pColorAttachmentFormats = &colourAttachmentFormat;
+}
+
+void PipelineBuilder::setDepthAttachmentFormat(VkFormat format)
+{
+    renderInfo.depthAttachmentFormat = format;
+}
+
+void PipelineBuilder::disableDepthTest()
+{
+    depthStencil.depthTestEnable = VK_FALSE;
+    depthStencil.depthWriteEnable = VK_FALSE;
+    depthStencil.depthCompareOp = VK_COMPARE_OP_NEVER;
+    depthStencil.depthBoundsTestEnable = VK_FALSE;
+    depthStencil.stencilTestEnable = VK_FALSE;
+    depthStencil.front = {};
+    depthStencil.back = {};
+    depthStencil.minDepthBounds = 0.0f;
+    depthStencil.maxDepthBounds = 1.0f;
+}
+
+VkPipeline PipelineBuilder::buildPipeline(VkDevice device)
+{
+    VkPipelineViewportStateCreateInfo viewportState{};
+    viewportState.sType= VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportState.viewportCount = 1;
+    viewportState.scissorCount = 1;
+
+    VkPipelineColorBlendStateCreateInfo colourBlending{};
+    colourBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colourBlending.logicOpEnable = VK_FALSE;
+    colourBlending.logicOp = VK_LOGIC_OP_COPY;
+    colourBlending.attachmentCount =  1;
+    colourBlending.pAttachments= &colourBlendAttachment;
+
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.pNext = &renderInfo;
+    pipelineInfo.stageCount = static_cast<uint32_t>(shaderStages.size());
+    pipelineInfo.pStages = shaderStages.data();
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterizer;
+    pipelineInfo.pMultisampleState = &multisampling;
+    pipelineInfo.pColorBlendState = &colourBlending;
+    pipelineInfo.pDepthStencilState = &depthStencil;
+    pipelineInfo.layout = pipelineLayout;
+
+    VkDynamicState state [] = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
+
+    VkPipelineDynamicStateCreateInfo dynamicInfo{};
+    dynamicInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamicInfo.dynamicStateCount = 2;
+    dynamicInfo.pDynamicStates = state;
+
+    pipelineInfo.pDynamicState = &dynamicInfo;
+
+    VkPipeline newPipeline;
+    if (vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &newPipeline)
+        != VK_SUCCESS)
+    {
+        LOG("Failed to create graphics pipeline");
+        return VK_NULL_HANDLE;
+    }
+
+    return newPipeline;
+}
+
+}  // namespace lonelycube::client
---
+++ src/client/graphics/vulkan/pipelines.h
@@ -0,0 +1,55 @@
+/*
+  Lonely Cube, a voxel game
+  Copyright (C) g 2024-2025 Bertie Cartwright
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "core/pch.h"
+
+#include <vulkan/vulkan_core.h>
+
+namespace lonelycube::client {
+
+class PipelineBuilder
+{
+public:
+    std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly;
+    VkPipelineRasterizationStateCreateInfo rasterizer;
+    VkPipelineColorBlendAttachmentState colourBlendAttachment;
+    VkPipelineMultisampleStateCreateInfo multisampling;
+    VkPipelineLayout pipelineLayout;
+    VkPipelineDepthStencilStateCreateInfo depthStencil;
+    VkPipelineRenderingCreateInfo renderInfo;
+    VkFormat colourAttachmentFormat;
+
+    PipelineBuilder() { clear(); }
+    VkPipeline buildPipeline(VkDevice device);
+
+    void clear();
+    void setShaders(VkShaderModule vertexShader, VkShaderModule fragmentShader);
+    void setInputTopology(VkPrimitiveTopology topology);
+    void setPolygonMode(VkPolygonMode mode);
+    void setCullMode(VkCullModeFlags cullMode, VkFrontFace frontFace);
+    void setMultisamplingNone();
+    void disableBlending();
+    void setColourAttachmentFormat(VkFormat format);
+    void setDepthAttachmentFormat(VkFormat format);
+    void disableDepthTest();
+};
+
+}  // namespace lonelycube::client
